22cd25ec4e8a19715af12b56790441e36b0f9c90
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2024-06-30 21:57:09 -0300

    Update 2024_07_01_005304_create_voluntario_table.php

--- meu_site/database/migrations/2024_07_01_005304_create_voluntario_table.php
+++ meu_site/database/migrations/2024_07_01_005304_create_voluntario_table.php
@@ -4,24 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateVoluntarioTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('voluntario', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('telefone');
+            $table->string('endereco');
+            $table->string('cidade');
+            $table->string('email')->unique();
+            $table->text('texto_livre')->nullable();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('voluntario');
     }
-};
+}
